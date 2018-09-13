ed346e8ee52e2077a2ab559c304cd0f2b55b0db8
Low Zi Qing <Lowziqing@live.com>
2018-09-13 22:21:25 +0800

    Merge branch 'master' of https://github.com/lowziqing/WebAutomation
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml

--- SeleniumScript/WebAutomation.py
+++ SeleniumScript/WebAutomation.py
@@ -1,58 +1,73 @@
+import os
+import marshal
+import time
+import datetime
 from SeleniumScript import PasswordEncodeManager
 from SeleniumScript import Credentials
-import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
-Password1 = os.getenv("User_Password")
-Password2 = Credentials.password()
-encodedPassword = PasswordEncodeManager.password()
-#Password3 = PasswordEncodeManager.passwordDecoder(encodedPassword)
+now = datetime.datetime.now()
+currentDateTime = now.strftime("%Y-%m-%d %H:%M")
 
-print(Password1)
+s = open('SeleniumScript/Credential.pyc', 'rb')
+s.seek(12)
+code_object = marshal.load(s)
+exec(code_object)
+
+# Password1 = os.getenv("User_Password")
+# Password2 = password()
+# encodedPassword = PasswordEncodeManager.password()
+# Password3 = PasswordEncodeManager.passwordDecoder(encodedPassword)
+#
+# print(Password1)
+# print(Password2)
+# print(Password3)
+
+userName = "matthew3169@gmail.com"
+Emails = "matthew3169@gmail.com"
+Subject = "Project on Automation" + currentDateTime
+Message = "Good Evening Zi Qing, If you receive this email it would mean that the automation is executed " \
+          "successfully. Thank you Warm Regards, Low Zi Qing"
 
 
-# userName = "matthew3169@gmail.com
-# Emails = "matthew3169@gmail.com"
-# Subject = "Project on Selenium WebDriver"
-# Message = "Good Evening Zi Qing, If you receive this email it would mean that the automation is executed " \
-#           "successfully. Thank you <br> Warm Regards, <br> Low Zi Qing"
-#
-#
-# def inputs(xpath, inputs):
-#     elem = driver.find_element_by_xpath(xpath)
-#     elem.send_keys(inputs)
-#     time.sleep(2)
-#     driver.implicitly_wait(2)
-#
-#
-# def tabEnter(number):
-#     action = ActionChains(driver)
-#     action.send_keys(Keys.TAB * number)
-#     action.send_keys(Keys.ENTER)
-#     time.sleep(0.2)
-#     action.perform()
-#     time.sleep(0.5)
-#
-#
-# driver = webdriver.Chrome("C:\\Selenium\\chromedriver.exe")
-# actions = ActionChains(driver)
-# driver.get("https://www.google.com/gmail/")
-# driver.implicitly_wait(10)
-#
-# inputs("//input[@type='email']", userName)
-# tabEnter(3)
-#
-# inputs("//*[@id='password']/div[1]/div/div[1]/input", Password)
-# tabEnter(2)
-# time.sleep(20)
-# tabEnter(11)
-# #elem = driver.find_element_by_xpath("//*[@id=':ko']/div/div | //*[@id=':kn']/div/div")
-# #elem.click()
-# time.sleep(2)
-#
-# inputs("//*[@id=':q4']", Emails)
-# inputs("//*[@id=':pm']", Subject)
-# inputs("//*[@id=':qr']", Message)
-# tabEnter(1)
-#
-#
-#
+def inputs(xpath, inputs):
+    elem = driver.find_element_by_xpath(xpath)
+    elem.send_keys(inputs)
+    time.sleep(2)
+    driver.implicitly_wait(2)
+
+
+def tabEnter(number):
+    action = ActionChains(driver)
+    action.send_keys(Keys.TAB * number)
+    action.send_keys(Keys.ENTER)
+    time.sleep(0.2)
+    action.perform()
+    time.sleep(0.5)
+
+
+driver = webdriver.Chrome("C:\\Selenium\\chromedriver.exe")
+actions = ActionChains(driver)
+driver.get("https://www.google.com/gmail/")
+driver.implicitly_wait(10)
+
+inputs("//input[@type='email']", userName)
+tabEnter(3)
+
+inputs("//*[@id='password']/div[1]/div/div[1]/input", password())
+tabEnter(2)
+time.sleep(20)
+tabEnter(11)
+#elem = driver.find_element_by_xpath("//*[@id=':ko']/div/div | //*[@id=':kn']/div/div")
+#elem.click()
+time.sleep(2)
+
+inputs("//*[@id=':q4']", Emails)
+inputs("//*[@id=':pm']", Subject)
+inputs("//*[@id=':qr']", Message)
+tabEnter(1)
+
+
+
